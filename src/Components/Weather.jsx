ee1ab91e1bc86b9bd4fa4b137e5f59c91d6011dc
Vikash-Bachhety <bachhetyv@gmail.com>
2024-02-22 18:35:37 +0530

    update

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,17 +1,16 @@
-import React, { useState, useRef } from 'react';
-import './Components.css';
-import {key} from "./Api.js"
-import Clear from '../assets/weather Images/clear.png';
-import Mist from '../assets/weather Images/mist.png';
-import Clouds from '../assets/weather Images/cloudy.png';
-import Rain from '../assets/weather Images/rainy.png';
-import Haze from '../assets/weather Images/haze.png';
-import Fog from '../assets/weather Images/fog.png';
-import Smoke from '../assets/weather Images/Smoke.png';
-
+import React, { useState, useRef } from "react";
+import "./Components.css";
+import { key } from "./Api.js";
+import Clear from "../assets/weather Images/clear.png";
+import Mist from "../assets/weather Images/mist.png";
+import Clouds from "../assets/weather Images/cloudy.png";
+import Rain from "../assets/weather Images/rainy.png";
+import Haze from "../assets/weather Images/haze.png";
+import Fog from "../assets/weather Images/fog.png";
+import Smoke from "../assets/weather Images/smoke.png";
 
 function Weather() {
-  const [city, setCity] = useState('');
+  const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState(null);
 
   const inputRef = useRef(null);
@@ -19,14 +18,16 @@ function Weather() {
 
   const handleSearch = async () => {
     try {
-      setCity('');
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`);
+      setCity("");
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`
+      );
       if (!response.ok) {
-        throw new Error('Failed to fetch weather data');
+        throw new Error("Failed to fetch weather data");
       }
       const data = await response.json();
       setWeatherData(data);
-      resultRef.current.scrollIntoView({ behavior: 'smooth' });
+      resultRef.current.scrollIntoView({ behavior: "smooth" });
     } catch (error) {
       console.error(error);
     }
@@ -34,19 +35,19 @@ function Weather() {
 
   const getWeatherIcon = (condition) => {
     switch (condition) {
-      case 'Clear':
+      case "Clear":
         return Clear;
-      case 'Clouds':
+      case "Clouds":
         return Clouds;
-      case 'Rain':
+      case "Rain":
         return Rain;
-      case 'Haze':
+      case "Haze":
         return Haze;
-      case 'Fog':
+      case "Fog":
         return Fog;
-      case 'Mist':
+      case "Mist":
         return Mist;
-        case 'Smoke':
+      case "Smoke":
         return Smoke;
       default:
         return null;
@@ -54,15 +55,21 @@ function Weather() {
   };
 
   return (
-    <div className='bg-gradient-to-r from-sky-500 to-indigo-500 flex flex-col sm:flex-row bg-center w-full bg-cover sm:h-screen mt-8 sm:fixed -z-10 left-0 font-serif tracking-wider'>
-      <div className='scrollLeft sm:w-1/4 w-11/12 h-32 sm:h-72 sm:p-4 mt-16 sm:mt-24 mb-3 sm:mb-28 flex flex-col sm:gap-y-4 mx-4 sm:m-24 bg-black rounded-lg bg-opacity-80 text-white border-black border-2 drop-shadow-sm'>
-        <h2 className='text-sm sm:text-2xl font-bold'>Weather</h2>
-        <p className='text-sm sm:text-lg sm:leading-relaxed sm:tracking-wider'>Stay Ahead of the Elements
-          Stay informed and prepared with real-time weather updates from API Galaxy. Whether you're planning outdoor activities, traveling, or simply curious about the forecast, we've got you covered.</p>
+    <div className="bg-gradient-to-r from-sky-500 to-indigo-500 flex flex-col sm:flex-row bg-center w-full bg-cover sm:h-screen mt-8 sm:fixed -z-10 left-0 font-serif tracking-wider">
+      <div className="scrollLeft sm:w-1/4 w-11/12 h-32 sm:h-72 sm:p-4 mt-16 sm:mt-24 mb-3 sm:mb-28 flex flex-col sm:gap-y-4 mx-4 sm:m-24 bg-black rounded-lg bg-opacity-80 text-white border-black border-2 drop-shadow-sm">
+        <h2 className="text-sm sm:text-2xl font-bold">Weather</h2>
+        <p className="text-sm sm:text-lg sm:leading-relaxed sm:tracking-wider">
+          Stay Ahead of the Elements Stay informed and prepared with real-time
+          weather updates from API Galaxy. Whether you're planning outdoor
+          activities, traveling, or simply curious about the forecast, we've got
+          you covered.
+        </p>
       </div>
 
-      <div className='animate h-52 w-5/6 sm:w-1/4 sm:h-2/3 drop-shadow-md ml-8 sm:ml-0 flex flex-col mb-3 sm:mb-0 sm:mt-6 sm:hover:scale-95 transition-all duration-1000 bg-black rounded-lg bg-opacity-60 pt-2 sm:py-6 border-x-2 border-y-4 border-white'>
-        <h1 className="font-bold text-sm sm:text-2xl sm:mb-2 font-sans text-white">Weather App</h1>
+      <div className="animate h-52 w-5/6 sm:w-1/4 sm:h-2/3 drop-shadow-md ml-8 sm:ml-0 flex flex-col mb-3 sm:mb-0 sm:mt-6 sm:hover:scale-95 transition-all duration-1000 bg-black rounded-lg bg-opacity-60 pt-2 sm:py-6 border-x-2 border-y-4 border-white">
+        <h1 className="font-bold text-sm sm:text-2xl sm:mb-2 font-sans text-white">
+          Weather App
+        </h1>
         <div className="flex flex-col w-full h-5/6 items-center">
           <div className="flex gap-x-2 sm:w-11/12 sm:h-10 m-4">
             <input
@@ -81,8 +88,11 @@ function Weather() {
             </button>
           </div>
 
-          <div id="result" className="flex flex-col h-40 sm:w-11/12 sm:h-5/6" ref={resultRef}>
-
+          <div
+            id="result"
+            className="flex flex-col h-40 sm:w-11/12 sm:h-5/6"
+            ref={resultRef}
+          >
             {weatherData && (
               <>
                 <div className="flex justify-center w-full h-2/3">
@@ -94,11 +104,21 @@ function Weather() {
                   />
                 </div>
                 <div className="flex flex-wrap sm:w-[350px] sm:h-[450px] sm:gap-y-4 sm:mt-5 pl-2 justify-around items-center sm:leading-relaxed font-sans tracking-wider text-sm sm:text-lg ">
-                  <p className="text-justify w-36 h-1/4 text-white">City: {weatherData.name}</p>
-                  <p className="text-justify w-36 h-1/4 text-white">Humidity: {weatherData.main.humidity}</p>
-                  <p className="text-justify w-36 h-1/4 text-white">Weather: {weatherData.weather[0].description}</p>
-                  <p className="text-justify w-36 h-1/4 text-white">Temperature: {weatherData.main.temp} °C</p>
-                  <p className="text-justify w-36 h-1/4 mt-3 text-white">Wind-Speed: {weatherData.wind.speed}</p>
+                  <p className="text-justify w-36 h-1/4 text-white">
+                    City: {weatherData.name}
+                  </p>
+                  <p className="text-justify w-36 h-1/4 text-white">
+                    Humidity: {weatherData.main.humidity}
+                  </p>
+                  <p className="text-justify w-36 h-1/4 text-white">
+                    Weather: {weatherData.weather[0].description}
+                  </p>
+                  <p className="text-justify w-36 h-1/4 text-white">
+                    Temperature: {weatherData.main.temp} °C
+                  </p>
+                  <p className="text-justify w-36 h-1/4 mt-3 text-white">
+                    Wind-Speed: {weatherData.wind.speed}
+                  </p>
                 </div>
               </>
             )}
@@ -106,9 +126,15 @@ function Weather() {
         </div>
       </div>
 
-      <div className='scrollRight sm:w-1/4 w-11/12 h-28 sm:h-72 sm:p-4  mb-28 flex flex-col sm:gap-y-4 mx-4 sm:m-24 bg-black rounded-lg bg-opacity-80 text-white border-black border-2 drop-shadow-sm'>
-        <h2 className='text-sm sm:text-2xl font-bold'>Location-Based Weather</h2>
-        <p className='text-sm sm:text-lg sm:leading-relaxed sm:tracking-wider'>Whether you're at home or exploring a new destination, our location-based weather feature provides you with accurate forecasts tailored to your current location.</p>
+      <div className="scrollRight sm:w-1/4 w-11/12 h-28 sm:h-72 sm:p-4  mb-28 flex flex-col sm:gap-y-4 mx-4 sm:m-24 bg-black rounded-lg bg-opacity-80 text-white border-black border-2 drop-shadow-sm">
+        <h2 className="text-sm sm:text-2xl font-bold">
+          Location-Based Weather
+        </h2>
+        <p className="text-sm sm:text-lg sm:leading-relaxed sm:tracking-wider">
+          Whether you're at home or exploring a new destination, our
+          location-based weather feature provides you with accurate forecasts
+          tailored to your current location.
+        </p>
       </div>
     </div>
   );
