78c19f1538b7437a03e3c24cde8f90a75af65339
Vikash Bachhety <144031359+Vikash-Bachhety@users.noreply.github.com>
2025-01-15 13:46:41 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,25 +4,22 @@ import "./Components.css";
 
 function Home() {
   return (
-    <div className="bg-gradient-to-r from-purple-500 to-sky-600 flex flex-col w-max-full w-min-sm border h-auto items-center mt-16 sm:mt-auto -z-10 left-0">
-      <div className="sm:mt-10 w-xl h-20 xl:h-10">
-        <h1 className="animate text base font-serif text-2xl lg:text-4xl mt-10 sm:mt-12 font-bold text-white hover:scale-105 transition-all duration-300">
+    <div className="common2 flex flex-col items-center justify-center w-max-full w-min-sm w-full border min-h-screen gap-14 -mt-10 -z-10 left-0">
+      <div className=" w-xl h-20 xl:h-10">
+        <h1 className="animate text base font-serif text-2xl lg:text-5xl mt-10 sm:mt-12 font-bold text-white hover:scale-105 transition-all duration-300">
           Welcome to API Galaxy
         </h1>
       </div>
       <div className="text-sm md:text-xl flex flex-wrap justify-around h-auto w-max-xl xl:mt-20 w-min-sm lg:pt-5 border-none leading-6 sm:leading-loose tracking-widest">
-        <h2 className="sm:shadow-black shadow-xl scrollLeft sm:w-sm xl:w-1/3 text-white sm:mt-14 h-1/4 p-3 xl:p-5 sm:p-8 font-serif hover:scale-95 transition-all duration-300">
+        <h2 className="sm:shadow-white shadow-md scrollLeft sm:w-sm xl:w-1/3 text-white h-1/4 p-3 xl:p-5 sm:p-8 font-serif hover:scale-95 transition-all duration-300">
           Welcome to API Galaxy, the celestial hub where data constellations
           converge to illuminate your digital ventures! Explore the boundless
           expanse of our digital universe, where the stars of currency
           conversion, weather forecasting, linguistic mastery, and secure
           password generation shine brightly in their own cosmic corners.
         </h2>
-        <div className="w-3/4 md:w-1/3 md:h-72 md:mb-40 h-1/4 mb-20">
-          <h2 className="animate text-2xl xl:text-4xl mb-2 sm:mb-4 font-serif text-white font-solid">
-            FEATURES
-          </h2>
-          <div className="shadow-black shadow-xl scrollRight flex flex-col pb-8 text-sm sm:text-2xl text-white font-semibold gap-y-2 text-center font-serif">
+        <div className="w-3/4 md:w-1/3 md:h-72 md:mb-40 h-1/4 mt-10 sm:mt-0">
+          <div className="sm:shadow-white shadow-md scrollRight flex flex-col pb-8 text-sm sm:text-xl text-white gap-y-2 text-center font-serif">
             <Links to="Weather" text="Weather" />
             <Links to="Dictionary" text="Dictionary" />
             <Links to="Currency" text="Currency" />
